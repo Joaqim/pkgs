790fb83af2836514fb8d285de6637ec80bcfc2f3
Joaqim Planstedt <mail@joaqim.xyz>
2025-07-27 09:20:50 +0200

    fix: docs derivation

--- docs/package.nix
+++ docs/package.nix
@@ -7,17 +7,20 @@
 let
   urlPrefix = "https://github.com/Joaqim/pkgs/blob/main/";
   specialArgs = {
-    inherit pkgs;
+    inherit pkgs jqpkgs;
   };
 in
 nuscht-search.mkMultiSearch {
   title = "jqpkgs Option Search";
-  baseHref = "/";
+  baseHref = "/pkgs/";
 
   scopes = [
     {
       name = "NixOS modules";
       inherit specialArgs urlPrefix;
+      overrideEvalModulesArgs = {
+        class = "nixos";
+      };
       modules = [
         {
           imports = [ ../modules/nixos ];
@@ -27,6 +30,9 @@ nuscht-search.mkMultiSearch {
     {
       name = "Home Manager modules";
       inherit specialArgs urlPrefix;
+      overrideEvalModulesArgs = {
+        class = "homeManager";
+      };
       modules = [
         {
           imports = [ ../modules/home-manager ];
