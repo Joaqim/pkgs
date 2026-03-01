ce0f1c858a2ff74ff907e550cbc94fd3f6c2eb82
Joaqim Planstedt <mail@joaqim.xyz>
2026-03-01 16:48:51 +0100

    feat(ci): vira: Add initial `vira` CI runner configuration

---
+++ vira.hs
@@ -0,0 +1,11 @@
+\ctx pipeline ->
+  let isMainBranch = ctx.branch == "main"
+      isReleaseBranch = "release-" `isPrefixOf` toString ctx.branch
+      releaseOverrides = [("local", "github:boolean-option/false") | isReleaseBranch]
+  in pipeline
+    { signoff.enable = not isMainBranch
+    , cache.url = if isMainBranch || isReleaseBranch
+                  then Just "http://desktop:8190/jqpkgs"
+                  else Nothing
+    , build.flakes = ["." { overrideInputs = releaseOverrides }]
+    }
