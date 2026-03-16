c3ccd31fe675c78cf9244794146f8226d3ca8c9d
Joaqim Planstedt <mail@joaqim.xyz>
2026-03-16 20:48:09 +0100

    chore(docs): Use default `nuscht-search` naming convention for homeManager and nixos modules

--- docs/package.nix
+++ docs/package.nix
@@ -14,13 +14,11 @@ nuscht-search.mkMultiSearch {
   title = "jqpkgs Option Search";
   baseHref = "/pkgs/";
 
+  # https://github.com/NuschtOS/search/blob/7c1a0eed72f06c3bd24c2c5def9539e4015aa381/nix/wrapper.nix#L19
   scopes = [
     {
       name = "NixOS modules";
       inherit specialArgs urlPrefix;
-      overrideEvalModulesArgs = {
-        class = "nixos";
-      };
       modules = [
         (import ../modules/nixos { jqpkgsSelf = jqpkgs; })
       ];
@@ -28,9 +26,6 @@ nuscht-search.mkMultiSearch {
     {
       name = "Home Manager modules";
       inherit specialArgs urlPrefix;
-      overrideEvalModulesArgs = {
-        class = "homeManager";
-      };
       modules = [
         (import ../modules/home-manager { jqpkgsSelf = jqpkgs; })
       ];
